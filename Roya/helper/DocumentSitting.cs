b820a2f452bb0c25d292ec65c0af979715096c67
A7med-Hamdi <ahmedhamdy102020@gmail.com>
2022-11-22 13:21:33 +0200

    DocummentSitting

---
+++ Roya/helper/DocumentSitting.cs
@@ -0,0 +1,32 @@
+﻿namespace Roya.helper
+{
+    public class DocumentSitting
+    {
+        public static string addFile(FormFile file, string folderName)
+        {
+            var pathFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Files", folderName);
+
+            var fileName = $"{Guid.NewGuid()}{file.FileName}";
+            var filePath = Path.Combine(pathFolder, fileName);
+
+            using var fs = new FileStream(filePath, FileMode.Create);
+
+            file.CopyTo(fs);
+
+            return fileName;
+
+
+        }
+
+        public static void deleteFile(string folderName ,string fileNmae)
+        {
+            
+            var pathFile = Path.Combine(Directory.GetCurrentDirectory(), folderName,fileNmae);
+            if (File.Exists(pathFile))
+            {
+                File.Delete(pathFile);
+            } 
+
+        }
+    }
+}
