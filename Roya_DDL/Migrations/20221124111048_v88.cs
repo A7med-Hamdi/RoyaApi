0d9e92f6a6b592613c68a9c25251d53f8c7e3021
mahmoud <you@example.com>
2022-11-26 00:47:08 +0200

    prrr2

---
+++ Roya_DDL/Migrations/20221124111048_v88.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Roya_DDL.Migrations
+{
+    public partial class v88 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FavoritLists_Products_ProductId",
+                table: "FavoritLists");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FavoritLists_ProductId",
+                table: "FavoritLists");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_FavoritLists_ProductId",
+                table: "FavoritLists",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FavoritLists_Products_ProductId",
+                table: "FavoritLists",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Roya_DDL/Migrations/20221124112403_v89.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Roya_DDL.Migrations
+{
+    public partial class v89 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_FavoritLists_ProductId",
+                table: "FavoritLists",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FavoritLists_Products_ProductId",
+                table: "FavoritLists",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FavoritLists_Products_ProductId",
+                table: "FavoritLists");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FavoritLists_ProductId",
+                table: "FavoritLists");
+        }
+    }
+}
